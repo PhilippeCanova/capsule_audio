6c1bff1650536edcd9d91aa2f06ee20c79dd49b8
Philippe Canova <canova.philippe@free.fr>
2020-09-19 19:15:45 +0200

    Version viable avec affichage des fichiers enregistrés

--- upload.php
+++ upload.php
@@ -4,18 +4,9 @@ echo ("<br>");
 
 $mp3File = $_FILES['file']['tmp_name'];
 
-var_dump($_FILES);
-echo ("<br>");
-
-var_dump($mp3File);
-echo ("<br>");
-
-
-$name = './data/test.ogg';
+$today = date("Ymd_His");
+$name = './data/test_'.$today.'.ogg';
 $data = file_get_contents($mp3File);
-
-var_dump($data);
-echo ("<br>");
 
 file_put_contents($name, $data);
 
